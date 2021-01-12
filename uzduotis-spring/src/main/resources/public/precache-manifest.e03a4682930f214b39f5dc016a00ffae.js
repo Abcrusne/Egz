bea24d7ef899fc7fffae4361c061d1333e741618
Abcrusne <rusne.pet@gmail.com>
2021-01-12 11:32:41 +0200

    spring and react sujungti

---
+++ uzduotis-spring/src/main/resources/public/precache-manifest.e03a4682930f214b39f5dc016a00ffae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd52fd48fba8068557003f5850fb88ee",
+    "url": "/uzduotis-spring/index.html"
+  },
+  {
+    "revision": "ac77d76d53528e8d72b0",
+    "url": "/uzduotis-spring/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "ac77d76d53528e8d72b0",
+    "url": "/uzduotis-spring/static/js/2.3cf57519.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/uzduotis-spring/static/js/2.3cf57519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7d8d62407762b7f985",
+    "url": "/uzduotis-spring/static/js/3.c2ec2812.chunk.js"
+  },
+  {
+    "revision": "f872daee2f8ad3ff90cb",
+    "url": "/uzduotis-spring/static/js/main.71b79d3a.chunk.js"
+  },
+  {
+    "revision": "9aa47a683d4aba0db65e",
+    "url": "/uzduotis-spring/static/js/runtime-main.edcb2a89.js"
+  },
+  {
+    "revision": "b231a657e771365f7e04148401860935",
+    "url": "/uzduotis-spring/static/media/img1.b231a657.jpg"
+  },
+  {
+    "revision": "4eb4419ff5ee8838fa2825fd939d5ea2",
+    "url": "/uzduotis-spring/static/media/img2.4eb4419f.jpg"
+  },
+  {
+    "revision": "5ae99eaa2f2dc1f794226fbf00b9665e",
+    "url": "/uzduotis-spring/static/media/img3.5ae99eaa.jpg"
+  }
+]);
